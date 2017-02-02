13e84559a3761cebaa830e464b6b1ac4f9358e79
Liew <soonhen18@gmail.com>
2017-02-02 13:21:57 +0000

    Initial Commit

---
+++ Study Coach/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Study Coach
+//
+//  Created by Liew Soon Hen on 02/02/2017.
+//  Copyright © 2017 Liew Soon Hen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
